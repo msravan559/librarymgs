5ce1bc17bb4da6e043c920f5eb4759259060a4d4
msravan559 <msravan559@gmail.com>
2023-04-03 23:15:29 +0530

    added links to our landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
 
@@ -8,7 +9,8 @@ const LandingPage = () => {
             <h1>Login as....?</h1>
             
             <div className="users">
-
+            <Link to="/admin-login">Admin login</Link>
+            <Link to="/user-login">User login</Link> 
             </div>
 
         </div>
